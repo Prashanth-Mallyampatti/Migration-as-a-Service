ec33d22abfd84c20868e3aa95f7d494640560401
Prashanth-Mallyampatti <prashanth.m1996@gmail.com>
2019-11-17 03:36:12 +0000

    Adding pns, mainBR creation script

--- provider.sh
+++ provider.sh
@@ -12,6 +12,9 @@ sudo virsh net-define mainBR.xml
 sudo virsh net-start mainBR
 sudo virsh net-autostart mainBR
 
+# Create pns
+sudo ip netns add pns
+
 # Create veth pair
 sudo ip link add p1 type veth peer name br
 ip link set dev br up
@@ -25,12 +28,10 @@ ip netns exec pns dhclient p1
 ip link set dev mainBR up
 
 # Rules to take care
-#rule_num_1=sudo iptables -t filter -L FORWARD --line-numbers | grep testBR | grep REJECT | awk -c '{print $1}' | awk 'NR==1'
-#rule_num_2=sudo iptables -t filter -L FORWARD --line-numbers | grep testBR | grep REJECT | awk -c '{print $1}' | awk 'NR==2'
+rule_num_1=`sudo iptables -t filter -L FORWARD -nv --line-numbers | grep testBR | grep REJECT | awk -c '{print $1}' | awk 'NR==1'`
+rule_num_2=`sudo iptables -t filter -L FORWARD -nv --line-numbers | grep testBR | grep REJECT | awk -c '{print $1}' | awk 'NR==2'`
 #echo "$rule_num_1"
 #echo "$rule_num_2"
 
-#if [! $rule_num_1 -eq 0];
-#   sudo iptables -t filter -D FORWARD $rule_num_1
-#   sudo iptables -t filter -D FORWARD $rule_num_1
-#fi
+sudo iptables -t filter -D FORWARD $rule_num_1
+sudo iptables -t filter -D FORWARD $rule_num_1
