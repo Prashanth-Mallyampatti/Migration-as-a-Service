81b9f72394565e1041332e19bd4f1d9f41a1322a
Prashanth-Mallyampatti <prashanth.m1996@gmail.com>
2019-12-04 18:36:46 -0800

    Adding tenant files

--- src/logiclayer/gui/gui.sh
+++ src/logiclayer/gui/gui.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-GUI_DIR="/Users/prashanthm/Migration-as-a-Service/src/northbound/gui"
-INFRA_DIR="/Users/prashanthm/Migration-as-a-Service/src/northbound/config_files/infrastructure/"
-MIG_DIR="/Users/prashanthm/Migration-as-a-Service/src/northbound/config_files/migration/"
-GUI_INFRA_DIR="/Users/prashanthm/Migration-as-a-Service/src/northbound/gui/output/infra/*"
-GUI_MIG_DIR="/Users/prashanthm/Migration-as-a-Service/src/northbound/gui/output/migrate/*"
+INFRA_DIR="/root/Migration-as-a-Service/src/northbound/config_files/infrastructure/"
+MIG_DIR="/root/Migration-as-a-Service/src/northbound/config_files/migration/"
+GUI_INFRA_DIR="/root/Migration-as-a-Service/src/northbound/gui/output/infra/*"
+GUI_MIG_DIR="/root/Migration-as-a-Service/src/northbound/gui/output/migrate/*"
 
 cp $GUI_INFRA_DIR $INFRA_DIR
 cp $GUI_MIG_DIR $MIG_DIR
+
+cd /root/Migration-as-Service
+git add .
+git commit -m "Adding tenant files"
+git push origin master
